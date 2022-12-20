6fe8730e15fdb344131eae914a03a3ccb91d138c
Marsel Denilson Lima <marsel.lima@L364.terraviva.com.br>
2022-12-20 08:23:08 -0300

    first commit

---
+++ src/theme/index.ts
@@ -0,0 +1,16 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#FFFFFD',
+    TEXT: '#380140',
+    ALERT: '#F20587',
+
+    PRIMARY_900: '#380140',
+    PRIMARY_800: '#F2059F',
+  },
+
+  FONTS: {
+    REGULAR: 'Roboto_400Regular',
+    MEDIUM: 'Roboto_500Medium',
+    BOLD: 'Roboto_700Bold',
+  },
+}
